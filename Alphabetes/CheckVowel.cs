6293813f49ede44f4339f5f7b5eab7e8aa4e1289
Aj007ar <aj007.ar@gmail.com>
2022-08-14 16:29:53 +0530

    [Ajay] Added: Check Vowel File

---
+++ Alphabetes/CheckVowel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabetes
+{
+    internal class CheckVowel
+    {
+        public void Vowel()
+        {
+            Console.WriteLine("Enter the alphabete for checking vowel or not");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            while (ch <= 'a' || ch >= 'z')
+            {
+                Console.WriteLine("Please enter alphabetes only");
+                ch = Convert.ToChar(Console.ReadLine());
+            }
+
+            if ((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u'))
+            {
+                Console.WriteLine("Alphabete is Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Alphabete is Not a Vowel");
+            }
+        }
+    }
+}
---
+++ Alphabetes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Alphabetes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n***********************Check Vowel or Not***************");
+            CheckVowel checkVowel = new CheckVowel();
+            checkVowel.Vowel();
+        }
+    }
+}
